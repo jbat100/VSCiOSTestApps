4c9b05d07ab9c4fea62ed5e5a0fd661496eef1ab
Jonathan Thorpe <johnny.frenchy@gmail.com>
2013-01-31 18:08:09 +0100

    [iOS] Added card.io SDK, created payment flow.

---
+++ RestTestApp/RestTestApp/Classes/PaymentViewController.h
@@ -0,0 +1,19 @@
+//
+//  PaymentViewController.h
+//  RestTestApp
+//
+//  Created by Jonathan Thorpe on 1/31/13.
+//  Copyright (c) 2013 Jonathan Thorpe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CardIO.h"
+
+@interface PaymentViewController : UIViewController <CardIOPaymentViewControllerDelegate>
+
+@property (nonatomic, strong) CardIOCreditCardInfo* creditCardInfo;
+
+- (IBAction)scanCard:(id)sender;
+
+@end
---
+++ RestTestApp/RestTestApp/Classes/PaymentViewController.m
@@ -0,0 +1,68 @@
+//
+//  PaymentViewController.m
+//  RestTestApp
+//
+//  Created by Jonathan Thorpe on 1/31/13.
+//  Copyright (c) 2013 Jonathan Thorpe. All rights reserved.
+//
+
+#import "PaymentViewController.h"
+
+@interface PaymentViewController ()
+
+//@property (nonatomic, strong) CardIOPaymentViewController *scanViewController;
+
+@end
+
+@implementation PaymentViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)scanCard:(id)sender {
+    
+    /*
+     *  Create a new one every time just to be sure nothing funny happens...
+     */
+    
+    CardIOPaymentViewController *scanViewController = nil;
+    
+    scanViewController = [[CardIOPaymentViewController alloc] initWithPaymentDelegate:self];
+    //scanViewController.appToken = @"116519433a614dd8bd56b0f3fd6c6c95"; // get your app token from the card.io website
+    
+    [self presentModalViewController:scanViewController animated:YES];
+}
+
+- (void)userDidCancelPaymentViewController:(CardIOPaymentViewController *)scanViewController {
+    NSLog(@"User canceled payment info");
+    // Handle user cancellation here...
+    [scanViewController dismissModalViewControllerAnimated:YES];
+}
+
+- (void)userDidProvideCreditCardInfo:(CardIOCreditCardInfo*)info inPaymentViewController:(CardIOPaymentViewController *)scanViewController {
+    // The full card number is available as info.cardNumber, but don't log that!
+    NSLog(@"Received card info. Number: %@, expiry: %02i/%i, cvv: %@.", info.redactedCardNumber, info.expiryMonth, info.expiryYear, info.cvv);
+    self.creditCardInfo = info;
+    // Use the card info...
+    [scanViewController dismissModalViewControllerAnimated:YES];
+}
+
+@end
